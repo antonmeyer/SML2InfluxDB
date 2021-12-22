237927b524c68a0866b900c9e9affe387423a2ec
Anton Zeppelin <anton.zeppelin@live.de>
2021-12-22 19:40:08 +0100

    2nd commit

--- src/main.cpp
+++ src/main.cpp
@@ -12,13 +12,15 @@
 PowerMeter pm2(UART_NUM_2, GPIO_NUM_16);  // UART_PIN_NO_CHANGE keep the defaults does not work
 PowerMeter pm1(UART_NUM_1, GPIO_NUM_36);    //default UART2 = GPIO_NUM_16
 
+unsigned long lastsend;
 void setup() {
 
   Serial.begin(115200);
   Serial.println("hallo sml reader here");
-  pm1.dataset1.alias = "VHWP"; //dirty pointer hack?
-  pm2.dataset1.alias = "VHHS";
+  pm1.dataset.alias = "VHWP";
+  pm2.dataset.alias = "VHHS";
 
+  Serial.print("Connecting to WiFi: ");
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED)
   {
@@ -29,19 +31,22 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   influxdb_init();
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
   if (pm2.handle_event() ) {
-    send2InfluxDb(pm2.dataset1);
+    send2InfluxDb(pm2.dataset);
   };
 
   if (pm1.handle_event() ) {
-    send2InfluxDb(pm1.dataset1);
+    send2InfluxDb(pm1.dataset);
   };
   
+  if ((millis()- lastsend) > 300000){ //every 5 min
+    lastsend = millis();
+    sendESP32_to_Influxdb();
+  }
   
 }
